2b5c47e7c985b542a54624c718e01c59983e8f8f
Abdliswi <engabdliswi@gmail.com>
2022-02-20 08:41:37 +0200

    add recursion task 10

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -2,7 +2,24 @@
 
 function division(number, dividedBy){
     // Write you logic here.
-    return;
+    if(dividedBy==number){
+        return 1;
+    }
+    if(number<dividedBy){
+        return 0;
+    }
+    if(number==0){
+        return 0;
+    }
+    if (dividedBy==0){
+        return 0;
+    }
+    if(dividedBy==1){
+        return number;
+    }
+    else{
+        return division(number-dividedBy,dividedBy)+1;
+    }    
 }
 
 /* Write a function that implement Math.pow(x,n) but using recursion
@@ -12,8 +29,10 @@ pow(2,4) = 16
 
 
 function pow(x,n){
-    // Write you logic here.
-    return;
+    if(n==0){
+        return 1;
+    }
+    return x*pow(x,n-1);
 }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
@@ -26,9 +45,12 @@ Write a function that take n as parameter and return the nth element in the Fibo
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n){
-    // Write you logic here.
-    return;
+    if(n<2){
+        return n;
+    }
+    return fibonacci(n-2)+fibonacci(n-1);
 }
+
 
 /* Optional 
 
